9953f0762e1bd4bdb5a828b54b2ebb63b1392e3b
rStar <starlove124760@gmail.com>
2021-09-05 12:47:01 +0800

    Made DataId ReadOnly and no longer HideInInspector

--- Assets/rStarTools/Scripts/ScriptableObjects/BaseClasses/UniqueId.cs
+++ Assets/rStarTools/Scripts/ScriptableObjects/BaseClasses/UniqueId.cs
@@ -1,18 +1,31 @@
+#region
+
 using System;
-using UnityEngine;
+using Sirenix.OdinInspector;
+
+#endregion
 
 namespace rStarTools.Scripts.ScriptableObjects.BaseClasses
 {
     [Serializable]
     public class UniqueId
     {
-        [HideInInspector]
+    #region Public Variables
+
+        [ReadOnly]
         public string DataId;
+
         public string DisplayName;
+
+    #endregion
+
+    #region Constructor
 
         public UniqueId()
         {
             DataId = Guid.NewGuid().ToString();
         }
+
+    #endregion
     }
 }
