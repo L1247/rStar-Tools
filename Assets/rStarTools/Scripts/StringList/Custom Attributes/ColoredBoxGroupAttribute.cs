8ae359a680e9c8b12acf82e316dba6fd6f8a86a4
rStar <starlove124760@gmail.com>
2021-09-25 15:50:10 +0800

    remove Unused construct

--- Assets/rStarTools/Scripts/StringList/Custom Attributes/ColoredBoxGroupAttribute.cs
+++ Assets/rStarTools/Scripts/StringList/Custom Attributes/ColoredBoxGroupAttribute.cs
@@ -12,12 +12,11 @@ namespace rStarTools.Scripts.StringList.Custom_Attributes
     {
     #region Public Variables
 
-        public bool   BoldLabel;
-        public float  R , G , B , A;
-        public string LabelText;
-        public bool   ShowIcon     { get; set; }
-        public int    MarginBottom { get; set; }
-        public int    MarginTop    { get; set; }
+        public bool  BoldLabel;
+        public float R , G , B , A;
+        public bool  ShowIcon     { get; set; }
+        public int   MarginBottom { get; set; }
+        public int   MarginTop    { get; set; }
 
     #endregion
 
@@ -37,25 +36,6 @@ namespace rStarTools.Scripts.StringList.Custom_Attributes
             B        = b;
             A        = a;
 
-            BoldLabel = boldLabel;
-        }
-
-        public ColoredBoxGroupAttribute(
-            string group ,
-            string label ,
-            float  r , float g , float b , float a ,
-            bool   showLabel   = true ,
-            bool   centerLabel = false ,
-            bool   boldLabel   = false ,
-            float  order       = 0 , bool showIcon = false) : base(group , showLabel , centerLabel , order)
-        {
-            ShowIcon = showIcon;
-            R        = r;
-            G        = g;
-            B        = b;
-            A        = a;
-
-            LabelText = label;
             BoldLabel = boldLabel;
         }
 
