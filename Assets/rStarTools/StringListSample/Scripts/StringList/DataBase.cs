a0a8c4a3a89af4b8a68b6cd7b134250c82e161c9
rStar <starlove124760@gmail.com>
2021-09-27 20:36:34 +0800

    change actorType to dataBase from UniqueId

--- Assets/rStarTools/StringListSample/Scripts/StringList/DataBase.cs
+++ Assets/rStarTools/StringListSample/Scripts/StringList/DataBase.cs
@@ -1,6 +1,7 @@
 #region
 
 using System;
+using JetBrains.Annotations;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -9,7 +10,7 @@ using UnityEngine;
 namespace rStarTools.Scripts.StringList
 {
     [Serializable]
-    public class DataBase<D> : IUniqueId where D : ScriptableObject , IDataOverview
+    public class DataBase<DO> : IUniqueId where DO : ScriptableObject , IDataOverview
     {
     #region Public Variables
 
@@ -18,11 +19,19 @@ namespace rStarTools.Scripts.StringList
 
     #endregion
 
+    #region Protected Variables
+
+        [UsedImplicitly]
+        protected string validateErrorMessage;
+
+    #endregion
+
     #region Private Variables
 
         [HideLabel]
         [SerializeField]
-        private UniqueId<D> uniqueId;
+        [ValidateInput("@ValidateAll()" , DefaultMessage = "@validateErrorMessage" , ContinuousValidationCheck = true)]
+        private UniqueId<DO> uniqueId;
 
     #endregion
 
@@ -36,6 +45,63 @@ namespace rStarTools.Scripts.StringList
         public void SetDisplayName(string newDisplayName)
         {
             uniqueId.SetDisplayName(newDisplayName);
+        }
+
+    #endregion
+
+    #region Protected Methods
+
+        protected virtual DO GetDataOverview()
+        {
+            var dataOverview = Utility.GetDataOverview<DO>();
+            return dataOverview as DO;
+        }
+
+        protected virtual bool ValidateAll()
+        {
+            validateErrorMessage = string.Empty;
+            var result         = ValidatorDataOverview();
+            if (result) result = ValidateDisplayName();
+            if (result) result = ValidateSameDisplayName();
+            if (result) result = ValidateOthers();
+            return result;
+        }
+
+        protected virtual bool ValidateDisplayName()
+        {
+            var displayName              = uniqueId.DisplayName;
+            var displayNameIsNullOrEmpty = string.IsNullOrEmpty(displayName);
+            if (displayNameIsNullOrEmpty)
+                validateErrorMessage = StringListDescription.DisplayNameIsEmpty;
+            return displayNameIsNullOrEmpty == false;
+        }
+
+        protected virtual bool ValidateOthers()
+        {
+            return true;
+        }
+
+        protected virtual bool ValidateSameDisplayName()
+        {
+            var displayName = uniqueId.DisplayName;
+            var ids         = GetDataOverview().GetAllUniqueId();
+            var isDisplayNameSame = ids.FindAll(_ =>
+            {
+                if (_ == null) return false;
+                var sameDisplayName = _.DisplayName == displayName;
+                return sameDisplayName;
+            }).Count > 1;
+            if (isDisplayNameSame)
+                validateErrorMessage = $"{StringListDescription.SameDisplayName}: {displayName}";
+            return isDisplayNameSame == false;
+        }
+
+        protected virtual bool ValidatorDataOverview()
+        {
+            var dataOverview                         = GetDataOverview();
+            var overviewIsNull                       = dataOverview == null;
+            if (overviewIsNull) validateErrorMessage = StringListDescription.OverviewIsNull;
+            return overviewIsNull == false;
         }
 
     #endregion
