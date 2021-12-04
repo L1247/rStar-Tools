47af23d9113716852abe6840fdc9eade37d35ab9
rStar <starlove124760@gmail.com>
2021-12-05 07:41:07 +0800

    modify scene

--- Assets/rStarTools/LoadDataFromGoogleSheet/Scripts/Main/SkillData.cs
+++ Assets/rStarTools/LoadDataFromGoogleSheet/Scripts/Main/SkillData.cs
@@ -1,6 +1,5 @@
 #region
 
-using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Utilities;
@@ -33,64 +32,6 @@ namespace LoadDataFromGoogleSheet
         {
             GoogleSheetService.LoadDataArray<SkillInfo>(url , infos => skillInfos = infos);
         }
-
-    #endregion
-    }
-
-    [Serializable]
-    internal class SkillInfo
-    {
-    #region Public Variables
-
-        public int UUID => uuid;
-
-        public string Accuracy => accuracy;
-
-        public string Description => description;
-
-        public string Name => name;
-
-        public string Power => power;
-
-        public string PP => pp;
-
-    #endregion
-
-    #region Private Variables
-
-        [PropertyOrder(1)]
-        [TableColumnWidth(30 , false)]
-        [SerializeField]
-        private int uuid;
-
-        [PropertyOrder(5)]
-        [TableColumnWidth(60 , false)]
-        [SerializeField]
-        private string accuracy;
-
-        [PropertyOrder(7)]
-        [SerializeField]
-        private string description;
-
-        [PropertyOrder(2)]
-        [TableColumnWidth(100 , false)]
-        [SerializeField]
-        private string name;
-
-        [PropertyOrder(4)]
-        [TableColumnWidth(40 , false)]
-        [SerializeField]
-        private string power;
-
-        [PropertyOrder(6)]
-        [TableColumnWidth(40 , false)]
-        [SerializeField]
-        private string pp;
-
-        [PropertyOrder(3)]
-        [TableColumnWidth(60 , false)]
-        [SerializeField]
-        private string type;
 
     #endregion
     }
---
+++ Assets/rStarTools/LoadDataFromGoogleSheet/Scripts/Main/SkillSceneReference.cs
@@ -0,0 +1,10 @@
+#region
+
+using UnityEngine;
+
+#endregion
+
+namespace LoadDataFromGoogleSheet
+{
+    public class SkillSceneReference : MonoBehaviour { }
+}
